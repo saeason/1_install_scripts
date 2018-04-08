d5372c2151b4c56d9f489807d8bf6a56ab352914
scott eason <saeason72@googlemail.com>
2018-04-08 08:57:40 +0100

    syncing

---
+++ unused_scripts/fix_mouse_cursor_breeze_snow.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+
+echo "Everywhere Breeze-snow as cursor"
+echo "Change cursor if you want"
+
+sudo cp -r Personal/settings/default/index.theme /usr/share/icons/default/
+
+
+echo "################################################################"
+echo "####           TO SEE THE EFFECT YOU MUST LOG OFF          ######"
+echo "################################################################"
