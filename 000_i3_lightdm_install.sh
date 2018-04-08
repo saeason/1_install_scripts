537150a304496fe5580883928eeb5e7b60ed126f
scott eason <saeason72@googlemail.com>
2018-04-08 17:47:01 +0100

    updating

--- 000_i3_lightdm_install.sh
+++ 000_i3_lightdm_install.sh
@@ -18,6 +18,6 @@ sudo systemctl set-default graphical.target
 
 echo
 echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
-echo "::: i3 is now instlaled, RESTART then install the other scripts :::::::::"
+echo "::: i3 is now instlaled, run script 001 then RESTART. :::::::::::::::::::"
 echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
 echo
