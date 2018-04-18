6524cea91e7d39bd0c9414b704f0dd732b19c369
saeason <saeason72@googlemail.com>
2018-04-18 21:51:29 +0100

    updating

--- 003_ohmyzsh.sh
+++ 003_ohmyzsh.sh
@@ -14,7 +14,7 @@ sudo pacman -S --noconfirm --needed curl
 sudo pacman -S --noconfirm --needed wget
 sudo pacman -S --noconfirm --needed zsh
 
-sudo chsh -s /usr/bin/zsh 
+sudo chsh scott -s /usr/bin/zsh 
 
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
