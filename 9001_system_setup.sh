1ad2b769c020da4571a8792da4aca892de0014f4
saeason <saeason72@googlemail.com>
2018-04-21 07:29:00 +0100

    updating

--- 9001_system_setup.sh
+++ 9001_system_setup.sh
@@ -31,15 +31,16 @@ cd /home/scott/Dropbox/Sync/1_install_scripts
 
 ./002_i3_symlink.sh
 
+nohup ./003_ohmyzsh.sh &
+
 ./004_zshrc_config_symlink.sh
 ./005_polybar_rofi_compton.sh
 ./006_vim.sh
-
 ./101_create_personal_folders.sh
 ./102_fonts.sh
 ./103_sound.sh
 ./104_bluetooth.sh
 ./105_network_discovery.sh
-./106_lightdm_and_themes.sh
-
+./401_use_all_cores.sh
+./403_lightdm_and_themes.sh
 ./701_git_configure.sh
