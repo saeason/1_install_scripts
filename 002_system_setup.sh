2aabdd6a4f5f146b18e217a3ffd6a30805d8990f
saeason <saeason72@googlemail.com>
2018-04-22 19:04:53 +0100

    updating

---
+++ 002_system_setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+# set -e causes the shell to exit if any subcommand or pipeline returns a non-zero status.
+
+echo
+echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo "::: Sym-linking the .Xresources file from Dropbox. ::::::::::::::::::::::"
+echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo
+sleep 2
+
+rm -f /home/scott/.Xresources
+ln -s /home/scott/Dropbox/Sync/.Xresources /home/scott/.Xresources
+
+echo
+echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo "::: Executing xrdb -load ~/.Xresources. :::::::::::::::::::::::::::::::::"
+echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo
+sleep 2
+
+xrdb -load ~/.Xresources
+
+echo
+echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo "::: Running scripts to install fonts and create personal folders. :::::::"
+echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo
+
+cd /home/scott/Dropbox/Sync/1_install_scripts
+
+./003_i3_symlink.sh
+
+./005_zshrc_config_symlink.sh
+./006_vim.sh
+./007_polybar_rofi_compton.sh
+
+./101_create_personal_folders.sh
+./102_fonts.sh
+./103_sound.sh
+./104_bluetooth.sh
+./105_network_discovery.sh
+./106_lightdm_and_themes.sh
---
+++ 003_i3_symlink.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+# set -e causes the shell to exit if any subcommand or pipeline returns a non-zero status.
+
+echo
+echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo "::: Sym-linking i3 from Dropbox to the /home directory. :::::::::::::::::"
+echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+sleep 2
+
+if [ -d ~/.i3 ]; then
+	if [ -L ~/.i3 ]; then
+		# symbolic link specific commands go here
+		rm ~/.i3
+	else
+		# directory commands go here
+		rm -rf ~/.i3
+	fi
+fi
+
+if [ -d ~/.config/i3 ]; then
+	if [ -L ~/./config/i3 ]; then
+		# symbolic link specific commands go here
+		rm ~/.config/i3
+	else
+		# directory commands go here
+		rm -rf ~/.config/i3
+	fi
+fi
+
+ln -s ~/Dropbox/Sync/.i3 /home/scott/
+
+echo
+echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo "::: i3 files successfully linked to the /home directory. ::::::::::::::::"
+echo ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo
